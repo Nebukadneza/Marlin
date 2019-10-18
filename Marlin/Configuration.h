52327da0a99797eb648dc6c5dd4e94ee1bf51959
Dario Ernst <dario@kanojo.de>
2019-10-18 22:24:53 +0200

    Make advance pause behave better

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1060,7 +1060,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 350
+#define Z_MAX_POS 300
 
 /**
  * Software Endstops
@@ -1110,7 +1110,7 @@
 
   // Set one or more commands to execute on filament runout.
   // (After 'M412 H' Marlin will ask the host to handle the process.)
-  #define FILAMENT_RUNOUT_SCRIPT "M125"
+  #define FILAMENT_RUNOUT_SCRIPT "M600"
 
   // After a runout is detected, continue printing this length of filament
   // before executing the runout script. Useful for a sensor at the end of
